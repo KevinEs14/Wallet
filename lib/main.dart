f0251cc4e98d7d62aa956ab661eb9ca47505be7d
KevinEs14 <escobarkev@gmail.com>
2022-11-02 23:57:33 -0400

    configuration and conexion with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:wall_et/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:wall_et/firebase_options.dart';
 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
   TextEditingController _username = TextEditingController();
   TextEditingController _password = TextEditingController();
 
